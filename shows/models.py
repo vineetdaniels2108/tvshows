e94d6d9720a44471928926ca29211985efc16fde
vineetdaniels2108 <vineetdaniels@gmail.com>
2021-03-30 08:09:29 -0700

    Updated code

--- shows/models.py
+++ shows/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class ShowManager(models.Manager):
+    def basic_validators(self, postData):
+        errors = {}
+        
+        if len(postData['title'])<2:
+            errors['title'] = 'Title should be greater than 2 characters'
+        if len(postData ['network'])<3:
+            errors['network'] = 'Network should be greater than 3 characters'
+        if len(postData['description'])<10:
+            errors['description'] = 'Description should be more than 10 character'
+        return errors 
+    
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -9,3 +22,5 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now= True)
+    objects = ShowManager()
+    
--- shows/views.py
+++ shows/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from shows.models import *
 
 # Create your views here.
@@ -12,9 +13,20 @@ def add_show_page(request):
     return render (request, 'add_show.html')
 
 def add_show(request):
-    new_show = Show.objects.create(title = request.POST['title'], network = request.POST['network'], release_date = request.POST['date'], description = request.POST['description'])
-    show_id = new_show.id
-    return redirect (f'/show/{show_id}')
+    
+    errors = Show.objects.basic_validators(request.POST)
+    
+    if len(errors) > 0:
+        for k,v in errors.items():
+            messages.error(request, v)
+            
+        return redirect('/show/new')
+    
+    else:
+    
+        new_show = Show.objects.create(title = request.POST['title'], network = request.POST['network'], release_date = request.POST['date'], description = request.POST['description'])
+        show_id = new_show.id
+        return redirect (f'/show/{show_id}')
 
 def show(request, show_id):
     show = Show.objects.get(id = show_id)
@@ -31,12 +43,23 @@ def show_edit_page(request, show_id):
 
 def edit_show (request, show_id):
     show = Show.objects.get(id = show_id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.release_date = request.POST['date']
-    show.description = request.POST['description']
-    show.save()
-    return redirect (f'/show/{show_id}')
+    errors = Show.objects.basic_validators(request.POST)
+    
+    if show.title == request.POST['title']:
+        errors['title_unique'] = 'Enter a unique title'
+        
+    if len(errors)>0:
+        for k,v in errors.items():
+            messages.error(request, v)
+        return redirect (f'/show/{show_id}/edit')
+    
+    else:
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release_date = request.POST['date']
+        show.description = request.POST['description']
+        show.save()
+        return redirect (f'/show/{show_id}')
 
 def delete_show (request, show_id):
     show = Show.objects.get(id = show_id)
